397aff541a9be51224f2cb0aa342cbe41ae9b42d
krtapia <68833219+MuricioSantiago22@users.noreply.github.com>
2022-05-13 12:40:23 -0500

    Add project
    Add project.

---
+++ app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.application
+
+object AppConstants {
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "7ef5fc6c5b78bbc14db374ecede31f34"
+}
